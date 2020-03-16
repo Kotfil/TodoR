4c993e72f855dd7ba55223099e0219844a9df30d
FilippK <filippkotenko@outlook.com>
2020-03-16 17:15:14 +0200

    Monday ToDoList traning 2

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
 import React from 'react';
 import './App.css';
 import TodoListHeader from './TodoListHeader';
-import TodoListFooter  from './TodoListFooter';
+import TodoListFooter from './TodoListFooter';
 import TodoListTasks from './TodoListTasks';
 
 
 //123
 
 class App extends React.Component {
+    tasks = [
+        {title: "JS", isDone: true},
+        {title: "CSS", isDone: true},
+        {title: "React", isDone: true},
+        {title: "SASS", isDone: false},
+        {title: "Phyton", isDone: false}
+];
+
+    filterValue='Completed'
+
     render = () => {
-        let tasks = [
-            {title: "JS", isDone: true},
-            {title: "CSS", isDone: true},
-            {title: "React", isDone: true},
-            {title: "SASS", isDone: false},
-            {title: "Phyton", isDone: false}
-        ]
 
         return (
-            <div className="App"> 
-            <div className="todoList">
-               <TodoListHeader />
-               <TodoListFooter />
-               <TodoListTasks tasks={tasks}/>
+            <div className="App">
+                <div className="todoList">
+                    <TodoListHeader/>
+                    <TodoListFooter filterValue={this.filterValue}/>
+                    <TodoListTasks tasks={this.tasks}/>
 
-                  </div>
-              </div>
+                </div>
+            </div>
 
         );
     }
--- src/TodoListFooter.js
+++ src/TodoListFooter.js
@@ -2,16 +2,21 @@ import React from 'react';
 
 class TodoListFooter extends React.Component {
     render = () => {
+
+
+        let classForAll = this.props.filterValue === "All" ? "filter-active" : "";
+        let classForCompleted = this.props.filterValue === "Completed" ? "filter-active" : "";
+        let classForActive = this.props.filterValue === "Active" ? "filter-active" : "";
+
         return (
-        <div className="todoListFooter">
-        <button>All</button>
-        <button>Completed</button>
-        <button>Active</button>
-        </div>
+            <div className="todoList-footer">
+                <button className={classForAll}>All</button>
+                <button className={classForCompleted}>Completed</button>
+                <button className={classForActive}>Active</button>
+            </div>
         );
     }
 }
-
 
 
 export default TodoListFooter;
